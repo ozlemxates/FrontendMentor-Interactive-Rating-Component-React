43aba54959b615b978e2835246a5327f0eab93cd
Özlem Ateş <ozlemxatess@outlook.com>
2023-08-11 15:22:41 +0300

    Rename files

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div>
+      <Header /> 
+      <div>
+        <h1>Hello Murat</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+    return (
+        <div>
+            Merhaba ben Header'ım.
+        </div>
+    )
+}
+
+export default Header;
